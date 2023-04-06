6f4e92163c427b0e2c09ae094c46d034abeef27c
Kyle Kryst <119367684+KyleKryst@users.noreply.github.com>
2023-04-05 20:13:43 -0700

    Updated server.js (#32)
    Updated the server.js to be able to use express.handlebars
    
    Signed-off-by: Kyle Kryst <119367684+KyleKryst@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const routes = require("./routes");
+const exphbs = require("express-handlebars");
+
 // import sequelize connection
 const sequelize = require("./config/connection");
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set("view engine", "handlebars");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 // turn on routes
 app.use(routes);
+
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now Listening"));
+  app.listen(PORT, () => console.log(`Now Listening On Port ${PORT}`));
 });
