d83d118b4c147ea4eb0777956baf430cf47b9c07
Jose Mauricio Cabrera Elias <jmcabrerae@gmail.com>
2023-05-22 21:19:44 -0700

    installed handlebars dependencies

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const routes = require("./routes");
-const exphbs = require("express-handlebars");
+
 const path = require("path");
 
 // import sequelize connection
@@ -8,14 +8,6 @@ const sequelize = require("./config/connection");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({});
-
-app.engine('handlebars', hbs.engine);
-app.set("view engine", "handlebars");
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
